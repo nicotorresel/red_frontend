42647c0658b877da18447a96a455f9c008504617
Walter <w.e.p.l.91@gmail.com>
2020-05-29 20:07:50 -0300

    Semantic UI installed and working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card>
+          <Image src={logo} wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>React js</Card.Header>
+            <Card.Meta>
+              <span className='date'></span>
+            </Card.Meta>
+            <Card.Description>
+              <p>Edit <code>src/App.js</code> and save to reload.</p>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+            </a>
+          </Card.Content>
+        </Card>
       </header>
     </div>
   );
